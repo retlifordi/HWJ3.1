b0acf49a707c347f1496f449f8236370511f53d9
Anastasiia Kosterkina <nastya.kosterkina@mail.ru>
2023-10-21 20:15:29 +0300

    one line entry

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,6 @@
 public class BonusMilesService {
     public int calculate(int price) {
-        int bonus;
-        bonus = price / 20;
+        int bonus = price / 20;
         return bonus;
     }
 
